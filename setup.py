35e7a66c1e3288d563b920f232e75f92943543c1
Reece Dunham <me@rdil.rocks>
2019-09-20 08:13:36 -0400

    Add requirements section to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,10 @@ setuptools.setup(
     url="https://cloudrepooss.github.io/download-analytics/",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    install_requires=[
+        "Flask>=1.0.2",
+        "Flask-HttpAuth==3.3.0",
+        "filehandlers>=2.7"
+    ]
 )
