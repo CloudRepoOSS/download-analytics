53fa3a351c4e542463dbdcdd9a2f09fff4987799
Reece Dunham <me@rdil.rocks>
2019-09-20 08:09:26 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="CloudRepoAnalytics",
+    version="1.0.0",
+    author="CloudRepo",
+    author_email="support@cloudrepo.io",
+    url="https://cloudrepooss.github.io/download-analytics/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False
+)
