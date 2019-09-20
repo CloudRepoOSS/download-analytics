b67246258417ae81f2e31645b548c8c8356ce785
Reece Dunham <me@rdil.rocks>
2019-09-20 12:18:33 -0400

    Add nice to have metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,56 @@
 import setuptools
 
+# Read the long description:
+with open("README.md", mode="r") as FILE_HANDLER:
+    LONG_DESCRIPTION = FILE_HANDLER.read()
+
+URLs = \
+    {
+        "Bug Tracker": "https://github.com/CloudRepoOSS/download-analytics/issues",
+        "Source Code": "https://github.com/CloudRepoOSS/download-analytics",
+        "License": "https://github.com/CloudRepoOSS/download-analytics/blob/master/LICENSE",
+    }
+
+CLASSIFIERS = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Framework :: Flask",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: Apache Software License"
+    "Intended Audience :: Developers",
+    "Topic :: System",
+    "Topic :: System :: Filesystems",
+    "Topic :: System :: Logging",
+    "Topic :: System :: Monitoring",
+    "Topic :: System :: Software Distribution",
+    "Topic :: Internet",
+    "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+    "Topic :: Internet :: WWW/HTTP :: Site Management",
+    "Topic :: Multimedia :: Graphics :: Presentation",
+    "Topic :: Utilities",
+    "Development Status :: 5 - Production/Stable",
+    "Natural Language :: English",
+]
+
 setuptools.setup(
     name="CloudRepoAnalytics",
+    description="Flask and Annie-based server to keep track of CloudRepo artifact download counts",
     version="1.0.0",
     author="CloudRepo",
     author_email="support@cloudrepo.io",
     url="https://cloudrepooss.github.io/download-analytics/",
     packages=setuptools.find_packages(),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="Apache 2.0",
     include_package_data=True,
     zip_safe=False,
+    project_urls=URLs,
+    classifiers=CLASSIFIERS,
     install_requires=[
         "Flask>=1.0.2",
         "Flask-HttpAuth==3.3.0",
